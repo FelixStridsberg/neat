dedb7e11ade9d7664f1cbc67d5c95300ad4e1f37
Felix Stridsberg <felixstridsberg@gmail.com>
2020-05-24 11:31:53 +0200

    Added test for evaluator.

---
+++ src/test/java/com/vadeen/neat/evaluator/SimpleEvaluatorTest.java
@@ -0,0 +1,38 @@
+package com.vadeen.neat.evaluator;
+
+import com.vadeen.neat.genome.Genome;
+import com.vadeen.neat.species.Species;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SimpleEvaluatorTest {
+
+    @Test
+    public void testSimpleEvaluator() {
+        // Simple evaluator that sets the genome id as fitness.
+        SimpleEvaluator simpleEvaluator = new SimpleEvaluator() {
+            @Override
+            public float evaluate(Genome genome) {
+                return genome.getId();
+            }
+        };
+
+        // Setup species.
+        Genome g1 = new Genome(1);
+        Genome g2 = new Genome(2);
+
+        Species species = new Species(1, g1);
+        species.addGenome(g2);
+
+        // Evaluate.
+        simpleEvaluator.evaluateAll(List.of(species));
+
+        // Verify.
+        assertThat(g1.getFitness(), is(1.0f));
+        assertThat(g2.getFitness(), is(2.0f));
+    }
+}
